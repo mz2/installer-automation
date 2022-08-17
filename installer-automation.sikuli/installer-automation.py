f4f1328802f46dc88480a24e43ac4a7514c456ed
Matias Piipari <matias.piipari@gmail.com>
2022-08-17 18:26:39 +0300

    Correction to waiting at the end

--- installer-automation.sikuli/installer-automation.py
+++ installer-automation.sikuli/installer-automation.py
@@ -1,39 +1,37 @@
-
-wait("1660741650330.png")
-assert("ubuntu_safe_graphics.png")
+wait("try_or_install_ubuntu.png", FOREVER)
+assert "ubuntu_safe_graphics.png"
 type(Key.DOWN)
 type(Key.ENTER)
 
-wait("jammy_jellyfish.png", 1200)
-click("installer_panel_icon.png", 1200)
-wait("install_ubuntu.png", 1200)
+wait("jammy_jellyfish.png", FOREVER)
+click("installer_panel_icon.png")
+
+wait("install_ubuntu.png", FOREVER)
 click("continue.png")
 click("type_here.png")
-type('dogflap')
+type("dogflap")
 click("continue.png")
 
-assert("use_wired_connection.png")
+assert "use_wired_connection.png"
 click("continue.png")
 
-assert("normal_installation.png")
-assert("minimal_installation.png")
+assert "normal_installation.png"
+assert "minimal_installation.png"
 click("continue.png")
 
-assert("erase_disk.png")
+assert "erase_disk.png"
 click("continue.png")
 
-assert("write_changes_to_disk.png")
-wait("1660739976913.png")
+assert "write_changes_to_disk.png"
+
+wait("1660739976913.png", FOREVER)
 click("start_installing.png")
-assert("where_are_you.png")
-
-
+assert "where_are_you.png"
 click("continue.png")
 
-assert("disabled_continue.png")
+assert "disabled_continue.png"
 click("your_name_placeholder.png")
 type("Dustin Henderson")
-
 click("1660732897015.png")
 type("a", Key.CTRL)
 
@@ -50,11 +48,9 @@ click("1660732681771.png")
 type("herptyderp")
 click("continue.png")
 
-wait("copying_files.png", 3200)
-wait("restart_into_ubuntu.png", 12800)
-
+assert("copying_files.png")
+wait("1660749559452.png", FOREVER)
 click("restart_into_ubuntu.png")
 
-wait("please_remove_installation_medium.png")
+wait("please_remove_installation_medium.png", FOREVER)
 type(Key.ENTER)
-
